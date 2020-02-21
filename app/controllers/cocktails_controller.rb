05776e834290886f56d81126add0b0890ae56743
Carms Ng <carmenhyng89@gmail.com>
2020-02-20 19:57:40 -0500

    app functional

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,62 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:search].nil?
+      @cocktails = Cocktail.all
+    else
+      search
+    end
+  end
+
+  def search
+    @search = params[:search]
+    @cocktails = Cocktail.where("lower(#{:name}) LIKE ?", "%#{@search.downcase}%")
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :url)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,41 @@
+class DosesController < ApplicationController
+  before_action :find_dose, only: [:edit, :update, :destroy]
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dose.update(dose_params)
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+end
